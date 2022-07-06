e3ee4dcd59eaf8e18e0875ec7bf58e4933d2512b
김재원 <kw2282222@gmail.com>
2022-07-06 12:36:58 +0900

    chore: main page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from '../components/Home/Home';
+import Footer from '../components/Footer/Footer';
+
+const HomePage = () => {
+    return (
+        <>
+            <Home />
+            <Footer />
+        </>
+    );
+};
+
+export default HomePage;
