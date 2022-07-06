a0dea3750b6592757850b615cac77baad59efb49
김재원 <kw2282222@gmail.com>
2022-07-06 17:22:17 +0900

    chore: about components

--- src/components/Home/About/About.jsx
+++ src/components/Home/About/About.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import importAll from '../../../utils/importAll';
+import Carousel from '../../Carousel/Carousel';
+import * as s from './About.style';
+
+import Button from '../../Button/Button';
+import { ThemeProvider } from 'styled-components';
+
+import { dark } from '../../../styles/Theme';
 
 const About = () => {
-    return <div>About</div>;
+    const images = importAll(require.context('../../../assets/Nfts/', false, /bighead-.*\.svg$/));
+
+    return (
+        <s.Section>
+            <s.Container>
+                <s.Box>
+                    <Carousel images={images} />
+                </s.Box>
+                <s.Box>
+                    <s.Title>Welcome To The Weirdos Club.</s.Title>
+                    <s.SubText>
+                        The WEIRDOS CLUB is a private collection of NFTs—unique digital
+                        collectibles. The Weirdos are stored as ERC-721 tokens on the Ethereum
+                        blockchain and hosted on IPFS.
+                    </s.SubText>
+                    <s.SubTextLight>
+                        With more than 200+ hand drawn traits, each NFT is unique and comes with a
+                        membership to an exclusive group of successful investors. Join an ambitious
+                        ever-growing community with multiple benefits and utilities.
+                    </s.SubTextLight>
+
+                    <s.ButtonContainer>
+                        <ThemeProvider theme={dark}>
+                            <Button
+                                text="JOIN OUR SLACK"
+                                link="https://app.slack.com/client/T03L89PJ54N/C03LW9MFSR2"
+                            />
+                        </ThemeProvider>
+                    </s.ButtonContainer>
+                </s.Box>
+            </s.Container>
+        </s.Section>
+    );
 };
 
 export default About;
---
+++ src/components/Home/About/About.style.js
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+    min-height: 100vh;
+    width: 100%;
+    background-color: ${props => props.theme.text};
+    color: ${props => props.theme.body};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+`;
+
+export const Container = styled.div`
+    width: 75%;
+    margin: 0 auto;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Box = styled.div`
+    width: 50%;
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Title = styled.h2`
+    font-size: ${props => props.theme.fontxxl};
+    text-transform: capitalize;
+    width: 80%;
+    color: ${props => props.theme.body};
+    align-self: flex-start;
+    margin: 0 auto;
+`;
+
+export const SubText = styled.p`
+    font-size: ${props => props.theme.fontlg};
+    width: 80%;
+    color: ${props => props.theme.body};
+    align-self: flex-start;
+    margin: 1rem auto;
+    font-weight: 400;
+`;
+
+export const SubTextLight = styled.p`
+    font-size: ${props => props.theme.fontmd};
+    width: 80%;
+    color: ${props => `rgba(${props.theme.bodyRgba}, 0.6)`};
+    align-self: flex-start;
+    margin: 1rem auto;
+    font-weight: 400;
+`;
+
+export const ButtonContainer = styled.div`
+    width: 80%;
+    margin: 1rem auto;
+    align-self: flex-start;
+`;
