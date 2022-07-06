d9c5678fbbbaa14512fff893367258965dc1c9c8
김재원 <kw2282222@gmail.com>
2022-07-06 17:22:34 +0900

    add: import images util function

---
+++ src/utils/importAll.js
@@ -0,0 +1,10 @@
+const importAll = r => {
+    let images = [];
+    r.keys().forEach((item, index) => {
+        images.push(r(item));
+    });
+
+    return images;
+};
+
+export default importAll;
