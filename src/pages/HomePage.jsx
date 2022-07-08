4c5b730d26d673fac1d346db07f5ca882c7aa8b5
kw22228 <hot5521@naver.com>
2022-07-08 17:20:10 +0900

    add: scrollTop button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from '../components/Home/Home';
 import Footer from '../components/Footer/Footer';
+import ScrollToTop from '../components/ScrollToTop/ScrollToTop';
 
 const HomePage = () => {
     return (
         <>
             <Home />
             <Footer />
+            <ScrollToTop />
         </>
     );
 };
