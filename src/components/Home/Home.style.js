ce3b41c081f90571f073567fe97fc0c3040d44e1
김재원 <kw2282222@gmail.com>
2022-07-10 14:37:54 +0900

    update: responsive

--- src/components/Home/Home.style.js
+++ src/components/Home/Home.style.js
@@ -1,4 +1,5 @@
 import styled, { keyframes } from 'styled-components';
+import { responsive } from '../../styles/variable';
 
 export const Section = styled.section`
     min-height: ${props => `calc(100vh - ${props.theme.navHeight})`};
@@ -9,13 +10,26 @@ export const Section = styled.section`
 
 export const Container = styled.div`
     width: 75%;
-    min-height: 80vh;
+    min-height: 100vh;
     margin: 0 auto;
     /* background-color: lightblue; */
 
     display: flex;
     justify-content: center;
     align-items: center;
+
+    @media (${responsive.res1}) {
+        width: 85%;
+    }
+
+    @media (${responsive.res2}) {
+        flex-direction: column-reverse;
+        width: 100%;
+        & > *:first-child {
+            width: 100%;
+            margin-top: 2rem;
+        }
+    }
 `;
 
 export const Box = styled.div`
@@ -49,6 +63,19 @@ export const Title = styled.h1`
     .text-3 {
         color: red;
     }
+
+    @media (${responsive.res1}) {
+        font-size: ${props => props.theme.fontxl};
+    }
+
+    @media (${responsive.res2}) {
+        align-self: center;
+        text-align: center;
+    }
+
+    @media (${responsive.res3}) {
+        width: 90%;
+    }
 `;
 
 export const SubTitle = styled.h3`
@@ -59,11 +86,27 @@ export const SubTitle = styled.h3`
     margin-bottom: 1rem;
     width: 80%;
     align-self: flex-start;
+
+    @media (${responsive.res2}) {
+        align-self: center;
+        text-align: center;
+    }
+
+    @media (${responsive.res3}) {
+        font-size: ${props => props.theme.fontmd};
+    }
 `;
 
 export const ButtonContainer = styled.div`
     width: 80%;
     align-self: flex-start;
+
+    @media (${responsive.res2}) {
+        align-self: center;
+        text-align: center;
+
+        margin: 0 auto;
+    }
 `;
 
 export const VideoContainer = styled.div`
@@ -72,6 +115,10 @@ export const VideoContainer = styled.div`
     video {
         width: 100%;
         height: auto;
+    }
+
+    @media (${responsive.res1}) {
+        min-width: 40vh;
     }
 `;
 
@@ -95,6 +142,23 @@ export const Round = styled.div`
 
         animation: ${rotate} 6s linear infinite reverse;
     }
+
+    @media (${responsive.res1}) {
+        position: fixed;
+        width: 4rem;
+        height: 4rem;
+        left: none;
+        right: 2rem;
+        top: 0.5rem;
+
+        z-index: 999;
+    }
+
+    @media (${responsive.res2}) {
+        width: 4rem;
+        height: 4rem;
+        top: 0.5rem;
+    }
 `;
 
 export const Circle = styled.span`
@@ -112,4 +176,10 @@ export const Circle = styled.span`
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+
+    @media (${responsive.res1}) {
+        width: 2rem;
+        height: 2rem;
+        font-size: ${props => props.theme.fontlg};
+    }
 `;
