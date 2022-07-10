845bdd629887bacba12e619fa6844200a0f5e2b6
김재원 <kw2282222@gmail.com>
2022-07-10 14:36:31 +0900

    add: 30em responsive size

--- src/styles/variable.js
+++ src/styles/variable.js
@@ -2,8 +2,12 @@
 
 const responsiveSize = {
     res1: '64em', //1024px
+    res2: '48em', //768px
+    res3: '30em',
 };
 
 export const responsive = {
     res1: `max-width: ${responsiveSize.res1}`,
+    res2: `max-width: ${responsiveSize.res2}`,
+    res3: `max-width: ${responsiveSize.res3}`,
 };
